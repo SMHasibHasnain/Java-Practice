8c1b45d5aa81c4709187060e649fde109a744130
S M Hasib Hasnain <hasnain23105341116@diu.edu.bd>
2024-08-24 12:16:29 +0600

    CHeck

--- src/com/hasibhasnain/oop/abstraction/one/Lion.java
+++ src/com/hasibhasnain/oop/abstraction/one/Lion.java
@@ -2,7 +2,7 @@ package com.hasibhasnain.oop.abstraction.one;
 
 import java.sql.SQLOutput;
 
-public class Lion extends Animal {
+public class Lion extends Animal{
     void sound() {
         System.out.println("Lion sound");
     }
